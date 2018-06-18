3623c8cd6d0d3b82097d920f51af128332c71ed4
kzhuo123 <league@WorthyMini-5.attlocal.net>
2018-06-18 16:34:50 -0700

    Coding Exercise 1 and 2

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -6,7 +6,27 @@ import org.jointheleague.graphical.robot.Robot;
 public class CodingExercise1 {
 
     public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+Robot rob = new Robot();
+    rob.setPenWidth(10);
+  rob.penDown();
+   rob.setSpeed(10);
+ rob.hide();
+   String color = JOptionPane.showInputDialog("What color do you want");
+    		if(color.equalsIgnoreCase("green")) {
+    		rob.setPenColor(0, 250, 0);
+    		}
+    		else if (color.equalsIgnoreCase("blue")) {
+        		rob.setPenColor(0, 0, 250);
+        		}
+    		else {
+    			rob.setRandomPenColor();
+    		}
+    		for(int i = 0; i < 4; i++) {
+    			rob.move(100);
+    		rob.turn(90);
+    		}
+
+    		// 3. ask the user what color they would like the Robot to draw
 
    	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
 
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,6 +1,7 @@
 
 
 /* Level 0 Exam: Coding  Exercise #2 */
+
 import javax.swing.JOptionPane;
 
 /**
@@ -9,8 +10,23 @@ import javax.swing.JOptionPane;
  * If they are over 30, tell them they are too old to play this game.
  **/
 
-public class CodingExercise2 {
 
+
+
+
+
+public class CodingExercise2 {
+public static void main(String[] args) {
+	String age = JOptionPane.showInputDialog("How old are you?");
+int a = Integer.parseInt(age);
+int d = 2018 - a;
+if(a > 30) {
+	JOptionPane.showMessageDialog(null, "You are too old to play! But, you were born in the year "+d+".");
+}
+else {
+	JOptionPane.showMessageDialog(null,"You were born in the year "+d+"." );
+}
+}
 }
 
 
